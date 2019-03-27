f03a165ff0b87dae3a5b2af33c700967608683f4
guakun01 <guakun01@gmail.com>
2019-03-27 17:24:37 +0800

    写完 header 中的 content 啦

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,9 +13,13 @@
           {{seller.description}} / {{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletin-wrapper"></div>
@@ -30,6 +34,17 @@ export default {
       type: Object,
     }
   },
+  data () {
+    return {
+      classMap: [
+        'decrease',
+        'discount',
+        'special',
+        'invoice',
+        'guarantee',
+      ]
+    };
+  },
 };
 </script>
 
@@ -38,17 +53,21 @@ export default {
 
 .header {
   color: #fff;
-  background-color: #000;
+  background-color: #999;
   .content-wrapper {
     padding: 24px 12px 18px 24px;
     font-size: 0;
+    position: relative;
     .avatar {
       display: inline-block;
+      vertical-align: top;
+      img {
+        border-radius: 2px;
+      }
     }
     .content {
       display: inline-block;
       margin-left: 16px;
-      font-size: 14px;
       .title {
         margin: 2px 0 8px 0;
         .brand {
@@ -65,6 +84,61 @@ export default {
           font-size: 16px;
           font-weight: bold;
         }
+      }
+      .description {
+        margin-bottom: 10px;
+        line-height: 12px;
+        font-size: 12px;
+      }
+      .support {
+        .icon {
+          display: inline-block;
+          vertical-align: top;
+          width: 12px;
+          height: 12px;
+          margin-right: 4px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+          &.decrease {
+            @include bg-image('decrease_1');
+          }
+          &.discount {
+            @include bg-image('discount_1');
+          }
+          &.guarantee {
+            @include bg-image('guarantee_1');
+          }
+          &.invoice {
+            @include bg-image('invoice_1');
+          }
+          &.special {
+            @include bg-image('special_1');
+          }
+        }
+        .text {
+          line-height: 12px;
+          font-size: 10px;
+        }
+      }
+    }
+    .support-count {
+      position: absolute;
+      right: 12px;
+      bottom: 14px;
+      padding: 0 8px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 14px;
+      background: rgba(0, 0, 0, .2);
+      text-align: center;
+      .count {
+        vertical-align: top;
+        font-size: 10px;
+      }
+      .icon-keyboard_arrow_right {
+        margin-left: 2px;
+        line-height: 24px;
+        font-size: 10px;
       }
     }
   }
