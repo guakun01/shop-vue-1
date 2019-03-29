047ce4260fb350ae829c45093506a2d5c62e1070
guakun01 <guakun01@gmail.com>
2019-03-29 17:19:01 +0800

    完成商家信息

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" v-el:seller>
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -30,11 +30,42 @@
         </ul>
       </div>
       <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li v-for="item in seller.supports" class="support-item border-1px">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" v-el:pic-wrapper>
+          <ul v-el:pic-list class="pic-list">
+            <li v-for="pic in seller.pics" class="pic-item">
+              <img :src="pic" width="120" height="90" alt="">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item border-1px" v-for="info in seller.infos">{{info}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
 import GStar from '../star/star';
 import Split from '../split/split';
 
@@ -43,10 +74,63 @@ export default {
   components: {
     GStar,
     Split,
+    BScroll,
   },
   props: {
     seller: {
       type: Object,
+    },
+  },
+  data() {
+    return {
+      classMap: [
+        'decrease',
+        'discount',
+        'special',
+        'invoice',
+        'guarantee',
+      ],
+      scroll: null,
+      picScroll: null,
+    };
+  },
+  watch: {
+    'seller'() {
+      this._initScroll();
+      this._initPicScroll();
+    },
+  },
+  ready() {
+    this._initScroll();
+    this._initPicScroll();
+  },
+  methods: {
+    _initPicScroll() {
+      if (this.seller.pics) {
+        let picWidth = 120;
+        let margin = 6;
+        let width = (picWidth + margin) * this.seller.pics.length - margin;
+        this.$els.picList.style.width = `${width}px`;
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$els.picWrapper, {
+              scrollX: true,
+              eventPassthorough: 'vertical',
+            });
+          } else {
+            this.picScroll.refresh();
+          }
+        });
+      }
+    },
+    _initScroll() {
+      if (!this.scroll) {
+        this.scroll = new BScroll(this.$els.seller, {
+          click: true,
+        });
+      } else {
+        this.scroll.refresh();
+      }
     },
   },
 };
@@ -111,6 +195,108 @@ export default {
           .stress {
             font-size: 24px;
           }
+        }
+      }
+    }
+    .bulletin {
+      padding: 18px 18px 0 18px;
+      .title {
+        margin-bottom: 8px;
+        line-height: 14px;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .content-wrapper {
+        padding: 0 12px 16px 12px;
+        @include border-1px(rgba(7, 17, 27, .1));
+        .content {
+          line-height: 24px;
+          font-size: 12px;
+          color: rgb(240, 20, 20);
+        }
+      }
+      .supports {
+        .support-item {
+          padding: 16px 12px;
+          @include border-1px(rgba(7, 17, 27, .1));
+          font-size: 0;
+          &:last-child {
+            @include border-none;
+          }
+          .icon {
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            vertical-align: top;
+            margin-right: 6px;
+            background-size: 16px 16px;
+            background-repeat: no-repeat;
+            &.decrease {
+              @include bg-image('decrease_4');
+            }
+            &.discount {
+              @include bg-image('discount_4');
+            }
+            &.guarantee {
+              @include bg-image('guarantee_4');
+            }
+            &.invoice {
+              @include bg-image('invoice_4');
+            }
+            &.special {
+              @include bg-image('special_4');
+            }
+          }
+          .text {
+            line-height: 16px;
+            font-size: 12px;
+            color: rgb(7, 17, 27);
+          }
+        }
+      }
+    }
+    .pics {
+      padding: 18px;
+      .title {
+        margin-bottom: 12px;
+        line-height: 14px;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .pic-wrapper {
+        width: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        .pic-list {
+          font-size: 0;
+          .pic-item {
+            display: inline-block;
+            margin-right: 6px;
+            width: 120px;
+            height: 90px;
+            &:last-child {
+              margin-right: 0;
+            }
+          }
+        }
+      }
+    }
+    .info {
+      padding: 18px 18px 0 18px;
+      color: rgb(7, 17, 27);
+      .title {
+        padding-bottom: 12px;
+        line-height: 14px;
+        font-size: 14px;
+        @include border-1px(rgba(7, 17, 27, .1));
+      }
+      .info-item {
+        padding: 16px 12px;
+        line-height: 16px;
+        @include border-1px(rgba(7, 17, 27, .1));
+        font-size: 12px;
+        &:last-child {
+          @include border-none;
         }
       }
     }
