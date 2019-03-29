a03a19e5e01fce95ab70d5419a5a90fec593c9ed
guakun01 <guakun01@gmail.com>
2019-03-29 15:49:41 +0800

    完成评论组件

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" v-el:ratings>
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
@@ -24,27 +24,121 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <rating-select :select-type="selectType" :only-content="onlyContent" 
+        :ratings="ratings"></rating-select>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-show="rateVisible(rating.rateType, rating.text)" v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar" width="28" height="28" alt="">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <g-star :size=24 :score="rating.score"></g-star>
+                <span class="delivery" v-if="rating.deliveryTime">
+                  {{rating.deliveryTime}} 分钟
+                </span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-if="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend" class="item">{{item}}</span>
+              </div>
+              <div class="time">
+                  {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
+import {formatDate} from 'common/js/date';
 import GStar from '../star/star';
+import Split from '../split/split';
+import RatingSelect from '../ratingselect/ratingselect';
+
+const ERR_OK = 0;
+const ALL = 2;
 
 export default {
   name: 'GRatingsPage',
   components: {
     GStar,
+    Split,
+    RatingSelect,
   },
   props: {
     seller: {
       type: Object,
     },
   },
+  data() {
+    return {
+      ratings: [],
+      selectType: ALL,
+      onlyContent: true,
+      scroll: null,
+    };
+  },
+  created() {
+    this.$http.get('/api/ratings')
+      .then(response => {
+        response = response.data;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$els.ratings, {
+              click: true,
+            });
+          });
+        }
+      });
+  },
+  events: {
+    'ratingType.select'(type) {
+      this.selectType = type;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    },
+    'content.toggle'(onlyContent) {
+      this.onlyContent = onlyContent;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    }
+  },
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDate(date, 'yyyy-MM-dd hh:mm');
+    },
+  },
+  methods: {
+    rateVisible(type, text) {
+      if (this.onlyContent && !text) {
+        return false;
+      }
+      if (this.selectType === ALL) {
+        return true;
+      } else {
+        return type === this.selectType;
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+  @import "../../common/scss/mixin.scss";
+
   .ratings {
     position: absolute;
     top: 174px;
@@ -118,6 +212,81 @@ export default {
           .delivery {
             margin-left: 12px;
             font-size: 12px;
+            color: rgb(147, 153, 159);
+          }
+        }
+      }
+    }
+    .rating-wrapper {
+      padding: 0 18px;
+      .rating-item {
+        display: flex;
+        padding: 18px 0;
+        @include border-1px(rgba(7, 17, 27, .1));
+        .avatar {
+          flex: 0 0 28px;
+          width: 28px;
+          margin-right: 12px;
+          img {
+            border-radius: 50%;
+          }
+        }
+        .content {
+          position: relative;
+          flex: 1;
+          .name {
+            margin-bottom: 4px;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(7, 17, 27);
+          }
+          .star-wrapper {
+            margin-bottom: 6px;
+            font-size: 0;
+            .star {
+              display: inline-block;
+              margin-right: 6px;
+              vertical-align: top;
+            }
+            .delivery {
+              display: inline-block;
+              vertical-align: top;
+              line-height: 12px;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+          }
+          .text {
+            margin-bottom: 9px;
+            line-height: 18px;
+            font-size: 12px;
+            color: rgb(7, 17, 27);
+          }
+          .recommend {
+            line-height: 16px;
+            font-size: 0;
+            .icon-thumb_up, .item {
+              display: inline-block;
+              margin: 0 8px 4px 0;
+              font-size: 9px;
+            }
+            .icon-thumb_up {
+              color: rgb(0, 160, 220);
+            }
+            .item {
+              padding: 0 6px;
+              border: 1px solid rgba(7, 17, 27, .1);
+              border-radius: 1px;
+              color: rgb(147, 153, 159);
+              background: #fff;
+            }
+          }
+          .time {
+            position: absolute;
+            top: 0;
+            right: 0;
+            line-height: 12px;
+            font-size: 10px;
             color: rgb(147, 153, 159);
           }
         }
